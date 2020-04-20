fae6be56953f8200ad713f75bfb36380df641a0d
criskgl <cris.kgl@gmail.com>
2020-04-20 20:06:28 +0200

    started bitmap

--- filesystem/filesystem.c
+++ filesystem/filesystem.c
@@ -10,21 +10,22 @@
  *
  */
 
+
 #include "filesystem/filesystem.h" // Headers for the core functionality
 #include "filesystem/auxiliary.h"  // Headers for auxiliary functions
 #include "filesystem/metadata.h"   // Type and structure declaration of the file system
-#include <string.h>
-	
-SuperBlock superblock_m;
+
+#include <stdlib.h>
+
+SuperBlock superblock;
 
 /*
  * @brief 	Generates the proper file system structure in a storage device, as designed by the student.
  * @return 	0 if success, -1 otherwise.
  */
 int mkFS(long deviceSize) { 
-	char block_buf[BLOCK_SIZE];
-	SuperBlock superblock;
-	INode inodes[MAX_FILES];
+
+	
 
 	if (deviceSize < MIN_DEV_SIZE || deviceSize > MAX_DEV_SIZE) return -1;
 
@@ -33,41 +34,30 @@ int mkFS(long deviceSize) {
 	superblock.inode_blocks = BLOCK_SIZE/sizeof(INode);
 	superblock.inodes = MAX_FILES;
 
-	for(int i=0; i<MAX_FILES; i++){
-		inodes[i] = (INode){NULL,USED,0,{}};
+	char bitmap[38];
+	
+	for(int i = 0; i < superblock.total_blocks; i++){
+		bitmap_setbit (bitmap, i, 0);
 	}
-
-	for(int i=0; i<sizeof(block_buf); i++){
-		block_buf[i] = 'X';
-	}
-
-	memcpy(&block_buf,&superblock,sizeof(SuperBlock));
-	memcpy(&block_buf[sizeof(SuperBlock)],&inodes,sizeof(INode)*MAX_FILES);
-
-	//fwrite(block_buf, 1, sizeof(block_buf), stdout);
-
-	if (bwrite(DEVICE_IMAGE, 0, ((char *)&(block_buf))) == -1) return -1; 	
-
+	//printf("%lu",sizeof(bitmap));
+	if (bwrite(DEVICE_IMAGE, 0, ((char *)&(superblock))) == -1) return -1; 	
 	return 0; 
 }
 
 /*
  * @brief 	Mounts a file system in the simulated device.
  * @return 	0 if success, -1 otherwise.
+ * leo info del disco y cargo las estructuras en memoria
  */
 int mountFS(void)
 {
-	char buffer[2048];
-	bread(DEVICE_IMAGE, 0, buffer );
- 
-	memcpy(&superblock_m, buffer, sizeof(SuperBlock));
-
-	return 0;
+	return -1;
 }
 
 /*
  * @brief 	Unmounts the file system from the simulated device.
  * @return 	0 if success, -1 otherwise.
+ * whatever we have in memory we need to put it in disk
  */
 int unmountFS(void)
 {
